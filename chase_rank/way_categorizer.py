1834e2dca7c9f550c20d3689be40cbfd2a035085
chris <58859629+christophschaller@users.noreply.github.com>
2022-11-22 23:42:09 +0100

    feat: add rulebased fallbacks to categorize way types

--- chase_rank/way_categorizer.py
+++ chase_rank/way_categorizer.py
@@ -1,12 +1,15 @@
 import overpy
 
-CATEGORIES = [
+WAY_CATEGORIES = [
     "street",
     "gravel",
     "trail",
-    "stairs"
+    "stairs",
+    "unknown"
 ]
 
+# https://taginfo.openstreetmap.org/keys/surface
+# surface is the most common tag we can use to determine a ways type
 WAY_SURFACE_MAP = {
     "street": [
         "asphalt",
@@ -38,14 +41,23 @@ SURFACE_WAY_MAP = {
     for surface in surface_list
 }
 
+# https://taginfo.openstreetmap.org/keys/highway
+# if the surface key is not set the highway key captures most ways
+# we can't know the surface for sure but can still sort most of the remaining ways
 WAY_HIGHWAY_MAP = {
     "dirt": [
         "path",
-        "track"
+        "track",
+        "unclassified"
     ],
     "street": [
-        "service"
-        "residential"
+        "service",
+        "residential",
+        "tertiary",
+        "secondary",
+        "primary",
+        "crossing",
+        "cycleway"
     ],
     "stairs": [
         "steps",
@@ -53,13 +65,48 @@ WAY_HIGHWAY_MAP = {
     ]
 }
 HIGHWAY_WAY_MAP = {
-    surface: way
+    highway: way
     for way, highway_list in WAY_HIGHWAY_MAP.items()
-    for surface in highway_list
+    for highway in highway_list
 }
 
+# https://taginfo.openstreetmap.org/keys/landuse
+# if no highway key is set the majority of remaining ways is captured by landuse
+# we don't know what the surface is, but we know what it is used for
+WAY_LANDUSE_MAP = {
+    "dirt": [
+        "farmland",
+        "grass",
+        "forest",
+        "meadow",
+        "orchard",
+        "farmyard",
+    ],
+    "street": [
+        "residential",
+        "industrial",
+    ]
+}
+LANDUSE_WAY_MAP = {
+    landuse: way
+    for way, landuse_list in WAY_LANDUSE_MAP.items()
+    for landuse in landuse_list
+}
+
+
 def categorize_way(way: overpy.Way) -> str:
+    """
+    Determines the category of a way and returns it as a string.
+    """
+    if not way:
+        return "unknown"
+
     tags = way.tags
     if "surface" in tags:
-        return WAY_SURFACE_MAP[tags["surface"]]
+        return SURFACE_WAY_MAP[tags["surface"]]
+    elif "highway" in tags:
+        return HIGHWAY_WAY_MAP[tags["highway"]]
+    elif "landuse" in tags:
+        return LANDUSE_WAY_MAP[tags["landuse"]]
 
+    return "unknown"
