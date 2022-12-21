2a699a8bac2876368c8cbd395bb21e9f8e44f9bf
chris <58859629+christophschaller@users.noreply.github.com>
2022-12-21 21:02:19 +0100

    refactor: keep valhalla image name in variable

--- bin/run_valhalla.ps1
+++ bin/run_valhalla.ps1
@@ -1,6 +1,5 @@
 $DataPath = "./data/osm/valhalla"
 $DataPath = Resolve-Path $DataPath
-$ValhallaImage = "ghcr.io/project-osrm/osrm-backend"
+$ValhallaImage = "docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest"
 
-
-docker run -p 8002:8002 -v "${DataPath}:/custom_files" -e serve_tiles=True  -e build_admins=True  docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest
+docker run -p 8002:8002 -v "${DataPath}:/custom_files" -e serve_tiles=True  -e build_admins=True $ValhallaImage
