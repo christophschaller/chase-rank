fbcfc43119d710938fe2111defd47b86eea561ad
davidbuu <78437983+davidbuu@users.noreply.github.com>
2022-12-21 21:16:40 +0100

    added strava api notebook
    changed run valalle.ps1

--- bin/run_valhalla.ps1
+++ bin/run_valhalla.ps1
@@ -1,6 +1,9 @@
-$DataPath = "./data/osm/valhalla"
+$DataPath = "/Users/david/dev_projects/chase-rank/data/osm/valhalla" #"./data/osm/valhalla"
 $DataPath = Resolve-Path $DataPath
 $ValhallaImage = "ghcr.io/project-osrm/osrm-backend"
 
 
-docker run -p 8002:8002 -v "${DataPath}:/custom_files" -e serve_tiles=True  -e build_admins=True  docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest
+docker run -p 8002:8002 -v "/Users/david/dev_projects/chase-rank/data/osm/valhalla:/custom_files" -e serve_tiles=True  -e build_admins=True  docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest
+
+
+#/Users/david/dev_projects/chase-rank/data/osm/valhalla
