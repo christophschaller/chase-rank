18e297b124c3c4b2a7c9f9305c39c44a0b4762c2
chris <58859629+christophschaller@users.noreply.github.com>
2022-12-22 11:20:13 +0100

    fix: revert absolute path to relative path

--- bin/run_valhalla.ps1
+++ bin/run_valhalla.ps1
@@ -1,8 +1,5 @@
-$DataPath = "/Users/david/dev_projects/chase-rank/data/osm/valhalla" #"./data/osm/valhalla"
+$DataPath = "./data/osm/valhalla"
 $DataPath = Resolve-Path $DataPath
 $ValhallaImage = "docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest"
 
-
-docker run -p 8002:8002 -v "${DataPath}:/custom_files" -e serve_tiles=True  -e build_admins=True  docker.pkg.github.com/gis-ops/docker-valhalla/valhalla:latest
-
-#/Users/david/dev_projects/chase-rank/data/osm/valhalla
+docker run -p 8002:8002 -v "${DataPath}:/custom_files" -e serve_tiles=True -e build_admins=True $ValhallaImage
