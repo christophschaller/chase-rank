cab4dfdccdae9fb0817d81b0fdf5c1327078004a
davidbuu <78437983+davidbuu@users.noreply.github.com>
2023-01-01 22:17:34 +0100

    add info to json

--- chase_rank/process.py
+++ chase_rank/process.py
@@ -7,6 +7,9 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
+import collections
+import time
+
 # TODO: könnte schon etwas dynamischer sein
 DATA_PATH = Path("../data")
 TEST_TRACK_PATH = Path(DATA_PATH, "routes/test_track.gpx")
@@ -89,6 +92,25 @@ def get_section_analytics(gpx_frame: gpd.GeoDataFrame) -> pd.DataFrame:
     section_df = pd.DataFrame(data=section_dict)
     return section_df
 
+def get_duration(points_df):
+    time_as_int = len(points_df)
+    time_as_time = time.strftime('%H:%M:%S', time.gmtime(time_as_int))
+    return time_as_time
+
+# TODO: Paused Time ist noch nicht korrekt. Diff zu SUM(Section Paused time), ebenso die Duration
+def get_pause_duration(points_df):
+    points_df['coordinates'] = points_df.apply(lambda row: (row['latitude'], row['longitude']), axis=1)
+    driving_time = points_df['coordinates'].nunique()
+    time_as_int = len(points_df)-driving_time
+    time_as_time = time.strftime('%H:%M:%S', time.gmtime(time_as_int))
+    return time_as_time
+
+def get_elevation_info(points_df):
+    diffs = points_df["elev"].diff()
+    total_asc = diffs[diffs > 0].sum()
+    total_desc = diffs[diffs < 0].sum()
+    return total_asc, total_desc
+
 
 # TODO: mach so als funktion auch keinen Sinn
 def save_match(points_df, section_analytics_df):
@@ -105,7 +127,13 @@ def save_match(points_df, section_analytics_df):
         "track_name": source_track_name,
         "date": start_date,
         # store distance without taking travel during pauses into account
-        "distance": distance
+        "distance": distance,
+        "surfaces": surface_stats(points_df),
+        "total_ascend":get_elevation_info(points_df)[0],
+        "total_descend":get_elevation_info(points_df)[1],
+        "duration": get_duration(points_df),
+        "paused_time": get_pause_duration(points_df),
+        #"average_kph": 
     }
 
     with open(Path(folder_path, "track_info.json"), "w") as file_pointer:
@@ -113,6 +141,22 @@ def save_match(points_df, section_analytics_df):
 
     points_df.to_csv(Path(folder_path, "points.csv"))
     section_analytics_df.to_csv(Path(folder_path, "sections.csv"))
+
+
+def surface_stats(gpx_frame: gpd.GeoDataFrame) -> dict:
+    surfaces = collections.defaultdict(lambda: 0.)# Distance(kilometers=0))
+    for index, surface_group in gpx_frame.groupby("surface_section"):
+        surface = surface_group["surface"].values[0]
+        # distance = group["distance"].dropna(axis=0).sum()
+        distance = surface_group["distance"].sum()
+        surfaces[surface] += distance
+
+    # remove distances travelled during pauses
+    for index, section_group in gpx_frame.groupby("section"):
+        surface = section_group["surface"].values[-1]
+        surfaces[surface] -= section_group["distance"].values[-1]
+
+    return dict(surfaces)
 
 
 def process_match(gpx_df, trace_df, edges_df):
